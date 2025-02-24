e423d12e811c0879d527810d31c35029ee5eb852
andresdanielmtz <autummata@gmail.com>
2025-02-23 19:16:43 -0600

    Add: Hello World Route to Order Repository

---
+++ order/src/main/java/com/example/order/controller/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.example.order.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloWorld {
+    @GetMapping("/")
+    public ResponseEntity<String> helloWorld(){
+        return ResponseEntity.ok("Hello World, Order");
+    }
+    
+}
